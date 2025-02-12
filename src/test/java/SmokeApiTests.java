0188f3b234ec2779c8e31129b71f8a3e99730a40
nataliamaslova <maslova.nd@gmail.com>
2025-02-12 21:53:32 +0100

    Add controller api tests

--- src/test/java/SmokeApiTests.java
+++ src/test/java/SmokeApiTests.java
@@ -1,4 +1,7 @@
+import controllers.UserController;
 import io.restassured.response.Response;
+import models.AddUserResponse;
+import models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.equalTo;
@@ -7,6 +10,8 @@ import static org.hamcrest.Matchers.notNullValue;
 import static io.restassured.RestAssured.given;
 
 public class SmokeApiTests {
+    UserController userController = new UserController();
+
     private static String baseUrl = "https://petstore.swagger.io/v2/";
     private static String body = """
             {
@@ -53,5 +58,33 @@ public class SmokeApiTests {
                 .body("code", equalTo(200))
                 .body("type", equalTo("unknown"))
                 .body("message", notNullValue(String.class));
+    }
+
+    @Test
+    void createUserControllerTest() {
+        User user = new User(0,
+                "username",
+                "firstName",
+                "lastName",
+                "email",
+                "password",
+                "phone",
+                0);
+        User userBuilder = User.builder()
+                .username("username")
+                .firstName("firstName")
+                .lastName("lastName")
+                .email("email")
+                .phone("password")
+                .userStatus(0)
+                .build();
+
+        Response response = userController.createUser(user);
+        AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(200, createdUserResponse.getCode());
+        Assertions.assertEquals("unknown", createdUserResponse.getType());
+        Assertions.assertFalse(createdUserResponse.getMessage().isEmpty());
     }
 }
